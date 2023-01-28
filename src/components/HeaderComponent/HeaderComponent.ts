a91fda4f7048b89c8f97534de357a41aae026535
lfilella97 <lfilella97@gmail.com>
2023-01-28 17:09:05 +0100

    add HeaderComponent

---
+++ src/components/HeaderComponent/HeaderComponent.ts
@@ -0,0 +1,18 @@
+import Component from "../Component/Component.js";
+import type ComponentStructure from "../Component/types";
+import { pokeLogo } from "./svgTitle.js";
+
+export class HeaderComponent extends Component implements ComponentStructure {
+  constructor(parentElement: Element) {
+    super(parentElement, "header", "header");
+  }
+
+  render() {
+    super.render();
+    this.domElement.innerHTML = `
+    
+    ${pokeLogo}
+
+`;
+  }
+}
