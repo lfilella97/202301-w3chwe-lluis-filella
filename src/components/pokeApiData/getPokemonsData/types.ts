dd083eade5ca0af7a0247a5085b0011350da93d4
lfilella97 <lfilella97@gmail.com>
2023-01-28 09:50:03 +0100

    add call to get pokemon data from Api, it's type and url

---
+++ src/components/pokeApiData/getPokemonsData/types.ts
@@ -0,0 +1,11 @@
+interface PokemonsListStructure {
+  name: string;
+  url: string;
+}
+
+export interface PokemonsApiRequestStructure {
+  count: number;
+  next: undefined;
+  previous: string;
+  results: PokemonsListStructure[];
+}
