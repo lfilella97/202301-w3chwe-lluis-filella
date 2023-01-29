fda0f9f392d29b54d3b54cd3a67dd34710b8fe88
lfilella97 <lfilella97@gmail.com>
2023-01-29 17:16:40 +0100

    delete getPokemonsData and makePokemonsList

--- src/pokeApiData/getPokemonsData/getPokemonsData.ts
+++
@@ -1,9 +0,0 @@
-import type { PokemonsApiRequestStructure } from "./types";
-
-export const getPokemonsData = async (
-  urlApi: string
-): Promise<PokemonsApiRequestStructure> => {
-  const response = await fetch(urlApi);
-  const results = (await response.json()) as PokemonsApiRequestStructure;
-  return results;
-};
