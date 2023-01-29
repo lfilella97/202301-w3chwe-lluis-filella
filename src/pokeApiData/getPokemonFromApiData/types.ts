80df31b2023374c5cf244799ad3160f227a52d27
lfilella97 <lfilella97@gmail.com>
2023-01-29 17:21:46 +0100

    add getPokemonFromApiData

---
+++ src/pokeApiData/getPokemonFromApiData/types.ts
@@ -0,0 +1,13 @@
+export interface PokemonListOfBasicCardInfoStructure {
+  species: {
+    name: string;
+  };
+  sprites: {
+    front_default: string;
+  };
+}
+
+export interface PokemonStructure {
+  pokemonName: string;
+  pokemonImage: string;
+}
