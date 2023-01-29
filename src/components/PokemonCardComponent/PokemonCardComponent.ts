f757c317cf679db725b881d7c460db42596b01d0
lfilella97 <lfilella97@gmail.com>
2023-01-29 21:17:38 +0100

    render card styled

--- src/components/PokemonCardComponent/PokemonCardComponent.ts
+++ src/components/PokemonCardComponent/PokemonCardComponent.ts
@@ -1,20 +1,19 @@
-import type { PokemonStructure } from "../../pokeApiData/getPokemonFromApiData/types.js";
+import type { PokemonStructure } from "../../pokeApiData/getPokemonFromApiData/types";
 import Component from "../Component/Component.js";
 import type { CardStructure } from "./types";
 
 export class PokemonCardComponent extends Component implements CardStructure {
   pokemon: PokemonStructure;
-  constructor(parentElement: Element) {
-    super(parentElement, "pokemonCard", "div");
+  constructor(parentElement: Element, pokemon: PokemonStructure) {
+    super(parentElement, "card", "div");
+    this.pokemon = pokemon;
   }
 
   render() {
     super.render();
     this.domElement.innerHTML = `
-    <img class="card__image" src="${
-      this.pokemon.pokemonImage
-    }" alt="front view of ${this.pokemon.pokemonName}" width="96" height="96">
-<span class="card__id">${0}</span>
+    <img class="card__image" src="${this.pokemon.pokemonImage}" alt="front view of ${this.pokemon.pokemonName}" width="96" height="96">
+
 <span class="card__name">${this.pokemon.pokemonName}</span>`;
   }
 }
