986683299da18517ec5215109e62b4aa7709e88c
lfilella97 <lfilella97@gmail.com>
2023-01-29 22:27:46 +0100

    change first pokemon from 2 to 1

--- src/pokeApiData/urlApi/urlApi.ts
+++ src/pokeApiData/urlApi/urlApi.ts
@@ -1,5 +1,5 @@
-export const pokemonsFrom = "1";
-export const pokemonsTo = "27";
+export const pokemonsFrom = "0";
+export const pokemonsTo = "20";
 
 export const urlApi = "https://pokeapi.co/api/v2/pokemon/";
 export const keyOffSet = `?offset=`;
